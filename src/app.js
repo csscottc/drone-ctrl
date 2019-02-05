a89b72533ff7f2aab669ce022002cacc66dfb7fb
Scott Carpenter <scottcarpenter@macbook-pro.lan>
2019-02-05 16:50:16 +0000

    Looking out for takeoff and land input

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 const readline = require("readline");
 const rl = readline.createInterface({ "input": process.stdin, "output": process.stdout});
+
+function handleInput(line) {
+    switch (line) {
+        case "takeoff":
+            console.log("Detected takeoff command.");
+            break;
+        case "land":
+            console.log("Detected land command.");
+            break;
+        default:
+            break;
+    }
+}
+
 console.log(`Lets get started!`);
 console.log(`Please enter a command:`);
-rl.on("line", line => console.log(line));
+rl.on("line",handleInput);
