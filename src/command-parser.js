7dba1d7d59c9223987114f700d6a33cd96fcba8f
Scott Carpenter <scottcarpenter01@gmail.com>
2019-02-18 19:54:06 +0000

    Making the config functions private in command parser

--- src/command-parser.js
+++ src/command-parser.js
@@ -32,54 +32,45 @@ function isFlip(line) {
 
 class CommandParser {
     constructor(config) {
-        this.onTakeoff = config.onTakeoff;
-        this.onLand = config.onLand;
-        this.onRight = config.onRight;
-        this.onLeft = config.onLeft;
-        this.onForward = config.onForward;
-        this.onBack = config.onBack;
-        this.onBattery = config.onBattery;
-        this.onFlip = config.onFlip;
-    }
-
-    parseCommand(line) {
-        if (isTakeoff(line)) {
-            this.onTakeoff();
-            return true;
+        this.parseCommand = function parseCommand(line) {
+            if (isTakeoff(line)) {
+                config.onTakeoff();
+                return true;
+            }
+            if (isLand(line)) {
+                config.onLand();
+                return true;
+            }
+            if(isForward(line)) {
+                const [, dist] = line.split(" ");
+                config.onForward(dist);
+                return true;
+            }
+            if(isBack(line)) {
+                const [, dist] = line.split(" ");
+                config.onBack(dist);
+                return true;
+            }
+            if(isRight(line)){
+                const [, dist] = line.split(" ");
+                config.onRight(dist);
+                return true;
+            }
+            if(isLeft(line)) {
+                const [, dist] = line.split(" ");
+                config.onLeft(dist);
+                return true;
+            }
+            if(isBattery(line)) {
+                config.onBattery(line);
+                return true;
+            }
+            if(isFlip(line)) {
+                config.onFlip(line);
+                return true;
+            }
+            return false
         }
-        if (isLand(line)) {
-            this.onLand();
-            return true;
-        }
-        if(isForward(line)) {
-            const [, dist] = line.split(" ");
-            this.onForward(dist);
-            return true;
-        }
-        if(isBack(line)) {
-            const [, dist] = line.split(" ");
-            this.onBack(dist);
-            return true;
-        }
-        if(isRight(line)){
-            const [, dist] = line.split(" ");
-            this.onRight(dist);
-            return true;
-        }
-        if(isLeft(line)) {
-            const [, dist] = line.split(" ");
-            this.onLeft(dist);
-            return true;
-        }
-        if(isBattery(line)) {
-            this.onBattery(line);
-            return true;
-        }
-        if(isFlip(line)) {
-            this.onFlip(line);
-            return true;
-        }
-        return false
     }
 }
 
